dfcb1f7beffb204d545b10fa4af8dff878499308
mauro.marcio <mmfla12@gmail.com>
2020-03-22 22:58:12 -0400

    projeto funcionando CleanArch Demo

---
+++ CleanArch.Domain/Interface/ICourseRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Interface
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+
+    }
+}
